d0298ce81049870b8f4d02f0c5a09e6a605f0394
Lex Linden <lex@lindenlab.com>
2011-04-05 17:25:03 +0000

    * Bugfixes * Implemented HostnameMixin * Implemented llclusto.get_by_hostname

--- src/llclusto/drivers/base/lindenequipment.py
+++ src/llclusto/drivers/base/lindenequipment.py
@@ -15,7 +15,7 @@ class LindenEquipment(Driver):
         clusto.begin_transaction()
 
         name, num = name_manager.allocator()
-        
+
         super(LindenEquipment, self).__init__(name, *args, **kwargs)
 
         name_manager.allocate(self, name)
@@ -23,11 +23,11 @@ class LindenEquipment(Driver):
         clusto.commit()
 
     def __repr__(self):
-        if hasattr(self, get_hostname):
+        if hasattr(self, "get_hostname"):
             return "%s(hostname=%s, name=%s, type=%s, driver=%s)" % (self.__class__.__name__,
                                                                      self.get_hostname(),
                                                                      self.entity.name,
                                                                      self.entity.type,
                                                                      self.entity.driver)
         else:
-            super(LindenEquipment, self).__repr__(self)
+            return super(LindenEquipment, self).__repr__()
