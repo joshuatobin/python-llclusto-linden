20766e81649a2cfe7f5704c8125a24edca25a5ea
Joshua Tobin <tobin@lindenlab.com>
2011-04-06 20:38:00 +0000

    Committing LindenRack Driver. Merging with Lex

--- src/llclusto/drivers/devices/servers/lindenserver.py
+++ src/llclusto/drivers/devices/servers/lindenserver.py
@@ -1,39 +1,39 @@
 import clusto
-from clusto.driver import *
+from clusto.drivers import *
 
 
-class LindenServer(LindenEquipment, BasicServer):
+class LindenServer(BasicServer):
     """
     LindenServer driver.
     """
     _clusto_type = "server"
-    _driver_name = "basicserver"
+    _driver_name = "lindenserver"
 
 
-class Class5Server(LindenServer):
-    """
-    """
-
-    _clusto_type = "server"
-    _driver_name = "class5server"
-
-
-class Class7Chassis(LindenServer):
-    """
-    """
-    pass
+#class Class5Server(LindenServer):
+#    """
+#    """
+#
+#    _clusto_type = "server"
+#    _driver_name = "class5server"
 
 
+#class Class7Chassis(LindenServer):
+#    """
+#    """
+#    pass
 
-class Class7Server(LindenServer, HostnameMixin):
-    """
-    """
 
-     _properties = {"server_class": None}
- 
-     def __init__(self, mac):
-        class7 = clusto.get_by_name("Class 7")
-        self.server_class = class7
+
+#class Class7Server(LindenServer):
+#    """
+#    """#
+
+#    _properties = {"server_class": None }
+# 
+#    def __init__(self, mac):
+#        class7 = clusto.get_by_name("Class 7")
+#        self.server_class = class7
 
     
 
--- src/llclusto/drivers/locations/racks/lindenracks.py
+++ src/llclusto/drivers/locations/racks/lindenracks.py
@@ -7,5 +7,49 @@ class LindenRack(BasicRack):
     """
     _clusto_type="rack"
     _driver_name="lindenrack"
+    
+    _properties = {'minu':1, 'maxu':45, 'pdu_mounts':4}
+
+    def get_attached_pdus(self):
+        """
+        Returns a list of PDUs attached to a rack.
+        """
+
+        return self.attr_values(key="_contains", subkey="pdu")
+
+    def attach_pdu(self, pdu):
+        """
+        Attach a PDU to a rack.
+        """
+        # change to lindenpdu
+        if not isinstance(pdu, LindenPDU):
+            raise TypeError("%s does not appear to be of valid PDU type." % pdu)
+
+        attached_pdus = self.get_attached_pdus()
+        
+        if len(attached_pdus) >= self.pdu_mounts:
+            raise Exception("You can only add up to %d PDUs in a rack. "
+                            "This rack already contains %d PDUs..." % (self.pdu_mounts, len(attached_pdus)))
+        
+        if pdu in attached_pdus:
+            raise Exception("%s already exists in rack: %s..." % (pdu, self.entity.name))
+        else:
+            self.add_attr("_contains", pdu, number=True, subkey="pdu")
+        
+    def detach_pdu(self, pdu):
+        """
+        Detach a PDU from a rack.
+        """
+        
+        attached_pdus = self.get_attached_pdus()
+        
+        if pdu not in attached_pdus:
+            raise Exception("%s not found attached to %s." % (pdu, self.name))
+        else:
+            self.del_attrs("_contains", value=pdu, subkey="pdu")
+
+
+
 
     
+
