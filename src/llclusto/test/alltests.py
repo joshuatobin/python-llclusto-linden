cfbca69fd3cdbff6c97481e4df756c6312c131d4
Michael Kania <dynamike@lindenlab.com>
2011-04-06 20:06:26 +0000

    * Copy unit test framework from clusto * Add unit tests for HostnameMixin

---
+++ src/llclusto/test/alltests.py
@@ -0,0 +1,47 @@
+
+if __name__ == '__main__':
+
+    import os
+    import sys
+
+
+    sys.path.append(os.path.realpath('.'))
+
+
+import unittest
+import llclusto.test
+
+
+def gettests(tests=None):
+    if not tests:
+        tests = ('llclusto.test.drivers',)
+
+    suite = unittest.defaultTestLoader.loadTestsFromNames(tests)
+
+    return suite
+
+
+def runtests(tests=None, db='sqlite:///:memory:', echo=False):
+
+    llclusto.test.testbase.DB=db
+    llclusto.test.testbase.ECHO=echo
+    suite = gettests(tests)
+    runner = unittest.TextTestRunner()    
+    runner.run(suite)
+
+
+
+
+if __name__ == '__main__':
+
+    import optparse
+
+    parser = optparse.OptionParser()
+    parser.add_option('--db', dest='dsn', 
+                      help='specifies which db to test against',
+                      default='sqlite:///:memory:')
+    parser.add_option('--echo', dest='echo', action='store_true', default=False,
+                      help="Echo sqlalchemy sql")
+    
+    (options, args) = parser.parse_args()
+    runtests(args, options.dsn, options.echo)
---
+++ src/llclusto/test/drivers/common/__init__.py
@@ -0,0 +1,1 @@
+from hostnametests import *
