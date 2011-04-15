7249585485f238206ed5f4961eaa6f91f2aa0a48
Lex Linden <lex@lindenlab.com>
2011-04-15 14:00:45 +0000

    Modified __init__.py files to import PGIImage and PGIImageTests

--- src/llclusto/drivers/__init__.py
+++ src/llclusto/drivers/__init__.py
@@ -3,7 +3,7 @@ from llclusto.drivers.common import *
 from llclusto.drivers.devices import *
 from llclusto.drivers.locations import *
 from llclusto.drivers.vendors import *
-
+from llclusto.drivers.pgi import *
 
 
 
