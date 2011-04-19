6545468ccc18b9258f99418b9980015ed51ad36f
Joshua Tobin <tobin@lindenlab.com>
2011-04-19 22:22:20 +0000

    Adding new drivers to init.py

--- src/llclusto/drivers/devices/servers/__init__.py
+++ src/llclusto/drivers/devices/servers/__init__.py
@@ -2,5 +2,5 @@ from lindenserver import LindenServer, RevertPGIImageError
 from database import DBX, DB2008, DB2009, DB2009SSD
 from serverclass import ServerClass
 from orca import Orca, Orca2009
-from class5 import Class5Server
+from class5 import Class5Server, Class5bServer, Class5DellF1C, Class5DellPowerEdge860
 from class7 import Class7Chassis, Class7Server, ChassisFullError
