0f858bfbb9c103dcd3a34d20ca7f1c25ed3bbb58
Lex Linden <lex@lindenlab.com>
2011-04-19 19:28:48 +0000

    Added 3 Class 5 variants.

--- src/llclusto/drivers/devices/servers/class5.py
+++ src/llclusto/drivers/devices/servers/class5.py
@@ -3,10 +3,36 @@ from lindenserver import LindenServer
 from serverclass import ServerClass
 
 class Class5Server(LindenServer):
-    """
+    """Driver for basic Class 5 Servers, and also acts as a superclass for all
+    of the fun Class 5 variants we seem to have accreted.
     """
 
     _driver_name = "class5server"
 
     _server_class_name = "Class 5"
+
+class Class5bServer(Class5Server):
+    """
+    """
+    
+    _driver_name = "class5bserver"
+    
+    _server_class_name = "Class 5b"
+    
+class Class5DellF1C(Class5Server):
+    """
+    """
+    
+    _driver_name = "class5dellf1cserver"
+    
+    _server_class_name = "Class 5 Dell F1C"
+
+class Class5DellPowerEdge860(Class5Server):
+    """
+    """
+    
+    _driver_name = "class5dellpoweredge860"
+    
+    _server_class_name = "Class 5 Dell PowerEdge 860"
+
 
